4dafc591ad42d215090d871042b6da8df5ba9fd8
Hughan Kleintjes <hughank@gmail.com>
2021-08-28 13:12:59 +0200

    Level 200 done

--- server.js
+++ server.js
@@ -2,14 +2,69 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = require('./exampleresponse.json');
 
 // GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  res.send(videos);
 });
+
+app.post("/", (req, res) => {
+  try {
+    const body = req.body;
+
+    const id = Date.now()
+    body.id = id;
+
+    videos.push(body);
+
+    res.send({
+      id
+    });
+  } catch (error) {
+    res.status(500).send({
+      "result": "failure",
+      "message": "Video could not be saved"
+    });
+  }
+});
+
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const numericId = parseInt(id)
+
+  const video = videos.find((video) => video.id === numericId)
+
+  res.json({ video: video });
+});
+
+app.delete("/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const numericId = parseInt(id)
+    if (!numericId) {
+      throw new Error('Invalid id')
+    }
+    console.log(numericId)
+
+    const updatedVideos = videos.filter((video) => video.id !== numericId)
+    videos = updatedVideos
+
+    res.send({});
+  } catch (error) {
+    res.status(500).json({
+      "result": "failure",
+      "message": "Video could not be deleted"
+    });
+  }
+});
+
+
+
+
